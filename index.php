85bb71d85340a4e63abebd6d5e8b00388ff5a224
Andrea Ligorio <andrea85kro@gmail.com>
2021-04-26 15:41:06 +0200

    header completato

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="dist/app.css">
+    <title>Google Faq</title>
+</head>
+
+<body>
+    <?php
+
+    ?>
+    <header>
+        <div class="header_top">
+            <div class="logo">
+                <img src="img/Google-Logo.png" alt="Logo Google">
+            </div>
+            <div class="text">
+                <h2>Privacy e termini</h2>
+            </div>
+        </div>
+        <div class="header_bottom">
+            <nav>
+                <ul>
+                    <li>Introduzione</li>
+                    <li>Norme sulla privacy</li>
+                    <li>Termini di servizio</li>
+                    <li>Tecnonologie</li>
+                    <li class="active">Domande frequenti</li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</body>
+
+</html>
